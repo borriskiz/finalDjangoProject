2b2bda0fad002f9b6f04c8aab97d42b95ef4b0e6
borriskiz <borisnechaev2004@yandex.ru>
2024-11-07 21:16:43 +0500

    final commit

--- coindb/models.py
+++ coindb/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Country(models.Model):
     CONTINENT_CHOICES = [
         ("Africa", "Africa"),
@@ -14,7 +15,6 @@ class Country(models.Model):
     name = models.CharField(max_length=255, verbose_name="Country Name")
     continent = models.CharField(max_length=20, choices=CONTINENT_CHOICES, verbose_name="Continent")
     code = models.CharField(max_length=10, verbose_name="Country Code", unique=True)
-
 
     class Meta:
         verbose_name = "Country"
@@ -67,6 +67,7 @@ class Shop(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class CoinCollection(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
